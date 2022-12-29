6bb19a9762dfa66aaa03a5fc5691516f07441e79
Mihail <mihatr2002@gmail.com>
2022-12-29 08:25:28 +0300

    all

---
+++ TenLab/Driver/DriverInstance.cs
@@ -0,0 +1,38 @@
+﻿
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+
+namespace TenLab.Driver
+{
+    public class DriverInstance
+    {
+        private static IWebDriver GetChromeDriver()
+        {
+            return new ChromeDriver(_driverPath, new ChromeOptions());
+        }
+        private static IWebDriver _webDriver { get; set; } = null!;
+
+        private static string _driverPath { get; set; } = @"C:\Users\Helen\Desktop\driver";
+
+        public static IWebDriver GetInstance()
+        {
+            if (_webDriver == null)
+            {
+                _webDriver = GetChromeDriver();
+                _webDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+                _webDriver.Manage().Window.Maximize();
+                _webDriver.Navigate().GoToUrl("https://www.contiki.com/en-us");
+            }
+            return _webDriver;
+        }
+
+        public static void TearDown()
+        {
+            _webDriver.Quit();
+            _webDriver = null;
+        }
+
+    }
+}
